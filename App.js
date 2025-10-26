33f29b7a7874c9583499c6d098ea5e5356ebadbf
irfanLegion <irfanimran1997@gmail.com>
2025-10-27 01:04:29 +0800

    Complete Ex1

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {/*Exercise 1*/}
+      <Text style={{ fontSize: 24 }}>RP Values</Text>
+      <Text style={{ color: "green" }}>Excellence</Text>
+      <Text style={{ backgroundColor: "yellow" }}>Customer-Centric</Text>
+      <Text style={{ fontStyle: "italic" }}>Integrity</Text>
+      <Text style={{ textAlign: "center" }}>Teamwork</Text>
+      <Text style={{ color: "white", backgroundColor: "black" }}>
+        Enterprising
+      </Text>
+      <StatusBar barStyle="default" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
